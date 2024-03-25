02d9cde7602e01a974b68e2842c6f17bf91a63be
Deepak <chaubeydeepak903@gmail.com>
2024-03-25 09:31:40 +0530

    updating changes

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,8 @@ const App = () => {
             <Navbar />
             <Routes>
                 <Route exact path="/" Component={Home} />
+                <Route exact path="/support" Component={Home} />
+
                 <Route exact path="/about" Component={About} />
                 <Route exact path="/services" Component={Services} />
                 <Route exact path="/contact" Component={Contact} />
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,9 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
-import Img_credit from "../Images/credit.jpeg"
+import Img_credit from "../Images/credit.jpeg";
+import { useNavigate } from "react-router-dom";
+
 
 
 const Contact = () => {
+
+    const Navigate = useNavigate();
+
+    const [userData, setUserData] = useState({
+        firstName: "",
+        lastName: "",
+        phone: "",
+        email: "",
+        address: "",
+        message: ""
+    })
+
+    const inputEvent = (event) => {
+        const { name, value } = event.target;
+        setUserData({ ...userData, [name]: value })
+    }
+
+    const [agreeChecked, setAgreeChecked] = useState(false);
+
+    // Connecting with forebase
+    const submitData = async () => {
+        const { firstName, lastName, phone, email, address, message } = userData;
+        const res = await fetch('https://supporwebapp-default-rtdb.firebaseio.com/userDataRecord.json', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstName, lastName, phone, email, address, message
+            }),
+        })
+
+        if (res) {
+            setUserData({
+                firstName: "",
+                lastName: "",
+                phone: "",
+                email: "",
+                address: "",
+                message: ""
+            })
+            alert("Data stored");
+        }
+        else {
+            alert('plz fill the Data')
+        }
+    }
+
+
+    const formValidation = (event) => {
+        event.preventDefault();
+        if (
+            userData.firstName.trim() === "" &&
+            userData.lastName.trim() === "" &&
+            userData.phone.trim() === "" &&
+            userData.email.trim() === "" &&
+            userData.address.trim() === "" &&
+            userData.message.trim() === "" &&
+            !agreeChecked
+        ) {
+            alert("Please fill in all fields and agree to the terms.");
+            Navigate(0);
+        }
+        else {
+            submitData()
+        }
+    };
+
+
     return (
         <>
             <section className="contactus-section">
@@ -24,38 +95,38 @@ const Contact = () => {
                                     <form method="post">
                                         <div className="row">
                                             <div className="col-12 col-lg-6 contact-input-field">
-                                                <input type="text" placeholder="First Name" className="form-control" />
+                                                <input type="text" placeholder="First Name" className="form-control" name="firstName" value={userData.firstName} onChange={inputEvent} />
                                             </div>
                                             <div className="col-12 col-lg-6 contact-input-field">
-                                                <input type="text" placeholder="Last Name" className="form-control" />
+                                                <input type="text" placeholder="Last Name" className="form-control" name="lastName" value={userData.lastName} onChange={inputEvent} />
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="col-12 col-lg-6 contact-input-field">
-                                                <input type="phone" placeholder="Phone Number" className="form-control" />
+                                                <input type="phone" placeholder="Phone Number" className="form-control" name="phone" value={userData.phone} onChange={inputEvent} />
                                             </div>
                                             <div className="col-12 col-lg-6 contact-input-field">
-                                                <input type="email" placeholder="Email" className="form-control" />
+                                                <input type="email" placeholder="Email" className="form-control" name="email" value={userData.email} onChange={inputEvent} />
                                             </div>
                                         </div>
 
                                         <div className="row">
                                             <div className="col-12 contact-input-field">
-                                                <input type="text" placeholder="Add Address" className="form-control" />
+                                                <input type="text" placeholder="Add Address" className="form-control" name="address" value={userData.address} onChange={inputEvent} />
                                             </div>
                                         </div>
 
                                         <div className="row">
                                             <div className="col-12">
-                                                <input type="text" placeholder="Enter your Message" className="form-control" />
+                                                <input type="text" placeholder="Enter your Message" className="form-control" name="message" value={userData.message} onChange={inputEvent} />
                                             </div>
                                         </div>
                                         <div class="form-check form-checkbox-style">
-                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" />
+                                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" onClick={() => setAgreeChecked(!agreeChecked)} />
                                             <label class="form-check-label" for="flexCheckChecked" className="main-hero-para">
                                                 I agree that the Tsquard can use this email ,phone to contact me in future and the all the information given by me is true
                                             </label>
-                                            <button type="submit" className="btn btn-style w-100 contact-btn">Submit Now</button>
+                                            <button type="submit" className="btn btn-style w-100 contact-btn" onClick={formValidation}>Submit Now</button>
                                         </div>
                                     </form>
                                 </div>
